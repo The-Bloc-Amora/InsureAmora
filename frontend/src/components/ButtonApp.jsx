b3d46b66f04a490ce861cb3a685d5d7d3f84a0f1
jeff-email <adakolejeffrey@gmail.com>
2023-03-07 15:54:30 +0100

    fixed state data

--- frontend/src/components/ButtonApp.jsx
+++ frontend/src/components/ButtonApp.jsx
@@ -11,7 +11,7 @@ const ButtonApp = (props, { styles }) => {
     //   Connect Wallet Test
     // </button>
     <ConnectWallet
-       colorMode="light"
+      colorMode="light"
       className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
     />
   );
