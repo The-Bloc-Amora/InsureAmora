16701d96a98b3129d04987d1514f5a6d4b285e2b
jeff-email <adakolejeffrey@gmail.com>
2023-03-08 11:35:20 +0100

    changed app name

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => (
   <section className={`${style.flexCenter} ${style.paddingY} flex-col`}>
     <div className={`${style.flexStart} md:flex-row flex-col mb-8 w-full`}>
       <div className="flex-1 flex flex-col justify-start mr-10">
-        <h1 className="text-gradient text-2xl font-bold">INSUREAMORA</h1>
+        <h1 className="text-gradient text-2xl font-bold">THEBA INSURA</h1>
         <div className="flex felx-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${style.paragraph} ml-2`}>
@@ -25,7 +25,7 @@ const Footer = () => (
     </div>
     <div className="w-full flex justify-between item-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright 2023 INSUREAMORA. All Rights Reserved.
+        Copyright 2023 THEBA INSURA. All Rights Reserved.
       </p>
       <div className="flex flex-row md:mt-0 mt-6">
       </div>
--- frontend/src/components/NavbarApp.jsx
+++ frontend/src/components/NavbarApp.jsx
@@ -9,7 +9,7 @@ const NavbarApp = () => {
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       {/* <img src={logo} alt="hookbank" className="w-[124px] h-[32px]" /> */}
-      <h1 className="text-gradient text-2xl font-bold">INSUREAMORA APP</h1>
+      <h1 className="text-gradient text-2xl font-bold">THEBA INSURA APP</h1>
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         <Link
           to="/"
