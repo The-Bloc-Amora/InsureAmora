6ad6702176609122ea7b98268e81d4883584adea
JnrLouis <chibuezelouis@gmail.com>
2023-03-08 14:52:58 +0100

    fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
+    "process.env": {},
     "global": {},
   },
   plugins: [react()],
+  optimizeDeps:{
+    esbuildOptions:{
+      plugins:[
+        esbuildCommonjs(['react-calendar','react-date-picker']) 
+      ]
+    }
+  }
 });
+
